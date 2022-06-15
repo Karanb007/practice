c21c98103aefba12d69d547cd63ca10dfa8bbcdf
karanb007 <karanbhoite33@gmail.com>
2022-06-15 14:47:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import BarChart from './components/BarChart';
+
+const App = () => {
+  
+  
+  return (
+    <div>
+      <BarChart/>
+    </div>
+  )
+};
+
+export default App;
